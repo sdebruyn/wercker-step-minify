e663d0589efe0ada6c2038dbb312d8ee1ec1779b
Sam Debruyn <code.s@muel.be>
2015-07-23 21:29:38 +0200

    node for older distros

--- run.sh
+++ run.sh
@@ -3,10 +3,23 @@ set -e
 
 # check if node is installed
 if [ "$(which node)" == "" ]; then
+
+    # check if curl is installed
+    if [ "$(which curl)" == "" ]; then
+        if [ "$(which apt-get)" != "" ]; then
+            apt-get update
+            apt-get install -y curl
+        else
+            yum install -y curl
+        fi
+    fi
+
+    # install node
     if [ "$(which apt-get)" != "" ]; then
-		apt-get update
+		curl --silent --location https://deb.nodesource.com/setup_0.12 | bash -
         apt-get install -y npm nodejs build-essential
     else
+        curl --silent --location https://rpm.nodesource.com/setup | bash -
         yum install -y nodejs npm gcc-c++ make
     fi
 fi
