f5528612c6228bf6ae018f946feec9d582217de2
Sam Debruyn <code.s@muel.be>
2015-07-23 21:16:24 +0200

    install node

--- run.sh
+++ run.sh
@@ -1,2 +1,15 @@
 #/bin/bash
 set -e
+
+# check if node is installed
+if [ "$(which node)" == "" ]; then
+    if [ "$(which apt-get)" != "" ]; then
+		apt-get update
+        apt-get install -y npm nodejs build-essential
+    else
+        yum install -y nodejs npm gcc-c++ make
+    fi
+fi
+
+# install the HTML minifier
+npm install html-minifier -g
