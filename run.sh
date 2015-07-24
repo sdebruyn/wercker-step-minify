0e6d307fc11023b328d6ffa19fc3ca244d65eb05
Sam Debruyn <code.s@muel.be>
2015-07-24 23:26:58 +0200

    moved some code to properties

--- run.sh
+++ run.sh
@@ -1,11 +1,32 @@
 #/bin/bash
 set -e
 
+# set amount of threads if not set
+CORES=`nproc`
+echo $CORES cores detected
+if [ ! -n "$WERCKER_MINIFY_THREADS" ]; then
+    export WERCKER_MINIFY_THREADS=$CORES
+fi
+echo running with $WERCKER_MINIFY_THREADS threads
+
+# set base directory to public if not set
+DEFAULTDIR="public"
+if [ ! -n "$WERCKER_MINIFY_BASEDIR" ]; then
+    export WERCKER_MINIFY_BASEDIR=$DEFAULTDIR
+fi
+
+# set arguments if not set
+DEFAULTARGS="--use-short-doctype --remove-style-link-type-attributes --remove-script-type-attributes --remove-comments --minify-css --minify-js --collapse-whitespace"
+if [ ! -n "$WERCKER_MINIFY_HTMLARGS" ]; then
+    export WERCKER_MINIFY_HTMLARGS="$DEFAULTARGS"
+fi
+
 # check if node is installed
 if [ "$(which node)" == "" ]; then
 
     # check if curl is installed
     if [ "$(which curl)" == "" ]; then
+        echo curl not installed, installing...
         if [ "$(which apt-get)" != "" ]; then
             apt-get update
             apt-get install -y curl
@@ -15,6 +36,7 @@ if [ "$(which node)" == "" ]; then
     fi
 
     # install node
+    echo node not installed, installing...
     if [ "$(which apt-get)" != "" ]; then
 		curl --silent --location https://deb.nodesource.com/setup_0.12 | bash -
         apt-get install -y npm nodejs build-essential
@@ -24,14 +46,11 @@ if [ "$(which node)" == "" ]; then
     fi
 fi
 
-# set amount of threads if not set
-CORES=`nproc`
-if [ ! -n "$WERCKER_MINIFY_THREADS" ]; then
-    export WERCKER_MINIFY_THREADS=$CORES
-fi
-
 # install the HTML minifier
+echo installing html-minifier with npm
 npm install html-minifier -g
 
 # minify all the HTML files
-find public -iname *.html -print0 | xargs -0 -P ${WERCKER_MINIFY_THREADS} -n 1 -I filename html-minifier --use-short-doctype --remove-style-link-type-attributes --remove-script-type-attributes --remove-comments --minify-css --minify-js --collapse-whitespace -o filename filename
+echo "minifying HTML files in $WERCKER_MINIFY_BASEDIR with arguments $WERCKER_MINIFY_HTMLARGS"
+
+find ${WERCKER_MINIFY_BASEDIR} -iname *.html -print0 | xargs -0 -P ${WERCKER_MINIFY_THREADS} -n 1 -I filename html-minifier ${WERCKER_MINIFY_HTMLARGS} -o filename filename
